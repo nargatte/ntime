260a5dad7776213f7b8c4204128f7cd6f9cd4530
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2017-11-05 15:17:01 +0100

    make the DB create

--- NTime/BaseCore/DataBase/Entities/Player.cs
+++ NTime/BaseCore/DataBase/Entities/Player.cs
@@ -34,13 +34,13 @@ namespace BaseCore.DataBase
         public int CompetitionId { get; set; }
         public virtual Competition Competition { get; set; }
 
-        public int ExtraPlayerInfoId { get; set; }
+        public int? ExtraPlayerInfoId { get; set; }
         public virtual ExtraPlayerInfo ExtraPlayerInfo { get; set; }
 
-        public int DistanceId { get; set; }
+        public int? DistanceId { get; set; }
         public virtual Distance Distance { get; set; }
 
-        public int AgeCategoryId { get; set; }
+        public int? AgeCategoryId { get; set; }
         public virtual AgeCategory AgeCategory { get; set; }
 
         public virtual ICollection<TimeRead> TimeReads { get; set; }
