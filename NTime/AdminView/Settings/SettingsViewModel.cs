f56be3450c86c9ebb44eb8ba193158cc832120cf
chudytom <tomasz.chudzik.pl@gmail.com>
2017-11-12 14:13:11 +0100

    Settings View ready

--- NTime/AdminView/Settings/SettingsViewModel.cs
+++ NTime/AdminView/Settings/SettingsViewModel.cs
@@ -11,10 +11,69 @@ namespace AdminView.Settings
         public SettingsViewModel()
         {
             TabTitle = "Ustawienia";
+            SaveChangesCmd = new RelayCommand(OnSaveChanges);
         }
+
+
+        private string _name = String.Empty;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+
+        private string _city = String.Empty;
+        public string City
+        {
+            get { return _city; }
+            set { SetProperty(ref _city, value); }
+        }
+
+
+        private DateTime _eventDate = new DateTime();
+        public DateTime EventDate
+        {
+            get { return _eventDate; }
+            set { SetProperty(ref _eventDate, value); }
+        }
+
+
+        private string _organiser = String.Empty;
+        public string Organiser
+        {
+            get { return _organiser; }
+            set { SetProperty(ref _organiser, value); }
+        }
+
+
+        private string _description = String.Empty;
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+
+
+        private string _link = String.Empty;
+        public string Link
+        {
+            get { return _link; }
+            set { SetProperty(ref _link, value); }
+        }
+
+
+        private void OnSaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
         public void DetachAllEvents()
         {
             throw new NotImplementedException();
         }
+
+        public RelayCommand SaveChangesCmd { get; private set; }
+        public event Action AddCompetitionRequested = delegate { };
     }
 }
