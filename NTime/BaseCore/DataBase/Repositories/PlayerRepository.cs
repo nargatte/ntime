474b61f562b23a1d5d4127d93afcbe5bbc9b3f30
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2017-11-20 00:55:12 +0100

    small fix

--- NTime/BaseCore/DataBase/Repositories/PlayerRepository.cs
+++ NTime/BaseCore/DataBase/Repositories/PlayerRepository.cs
@@ -19,7 +19,7 @@ namespace BaseCore.DataBase
             items.OrderBy(i => i.LastName);
 
         protected override IQueryable<Player> GetIncludeQuery(IQueryable<Player> items) =>
-            items.Include(p => p.Distance).Include(p => p.AgeCategory);
+            items.Include(p => p.Distance).Include(p => p.AgeCategory).Include(p => p.ExtraPlayerInfo);
 
         public async Task SetSelectedStartTimeAsync(Player[] players, DateTime startTime)
         {
