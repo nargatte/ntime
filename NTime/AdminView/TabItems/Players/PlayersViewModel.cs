c49d663f5501755950dc2c4a7406fb72901f4d97
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2017-11-20 18:19:14 +0100

    no message

--- NTime/AdminView/TabItems/Players/PlayersViewModel.cs
+++ NTime/AdminView/TabItems/Players/PlayersViewModel.cs
@@ -140,7 +140,8 @@ namespace AdminView.Players
         private void Player_UpdateRequested(object sender, EventArgs e)
         {
             var playerToUpdate = sender as EditablePlayer;
-            Task.WaitAll(_playerRepository.UpdateAsync(playerToUpdate.DbEntity, playerToUpdate.DbEntity.Distance, playerToUpdate.DbEntity.ExtraPlayerInfo));
+            _playerRepository.UpdateAsync(playerToUpdate.DbEntity, playerToUpdate.DbEntity.Distance,
+                playerToUpdate.DbEntity.ExtraPlayerInfo);
         }
 
         private async void OnAddPlayerAsync()
