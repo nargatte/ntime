d4c82d2a612cee54f89e7a59a5a8a97fd8f59dd2
chudytom <tomasz.chudzik.pl@gmail.com>
2018-03-09 15:04:52 +0100

    Finally got Ng-boostrap working in Angular. Fixes in systemjs were needed. Key line: '@ng-bootstrap/ng-bootstrap': { main: '/bundles/ng-bootstrap.js', defaultExtension: 'js' },

--- NTime/Server/AngularSrc/systemjs.config.js
+++ NTime/Server/AngularSrc/systemjs.config.js
@@ -24,12 +24,19 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
+
+      'moment': 'node_modules/moment/moment.js',
+      //'@ng-bootstrap': 'node_modules/@ng-bootstrap',
+      '@ng-bootstrap/ng-bootstrap': '../node_modules/@ng-bootstrap/ng-bootstrap',
+      //'@ng-bootstrap/ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
+      //'@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
 
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      //'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      //'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -41,9 +48,13 @@
           }
         }
       },
-      rxjs: {
+      rxjs: {   
         defaultExtension: 'js'
-      }
+      },
+      //'@ng-bootstrap/ng-bootstrap': { main: 'index.js', defaultExtension: 'js' },
+      '@ng-bootstrap/ng-bootstrap': { main: '/bundles/ng-bootstrap.js', defaultExtension: 'js' },
     }
+
+
   });
 })(this);
