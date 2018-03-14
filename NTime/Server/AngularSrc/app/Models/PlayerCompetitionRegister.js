a33b7a898ae0929f10f214dbc256ea2aadd153ea
Unknown <krzysiak.grzegorz@gmail.com>
2018-03-14 17:39:36 +0100

    Angular integrated with asp.net
    and removes old files

--- NTime/Server/AngularSrc/app/Models/PlayerCompetitionRegister.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PlayerCompetitionRegister = /** @class */ (function () {
-    function PlayerCompetitionRegister() {
-    }
-    return PlayerCompetitionRegister;
-}());
-exports.PlayerCompetitionRegister = PlayerCompetitionRegister;
-//# sourceMappingURL=PlayerCompetitionRegister.js.map
