10a5177d225608741e2d5e756e8a24aeb0852a47
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2018-04-14 18:02:45 +0200

    ConfirmEmail

--- NTime/Server/App_Start/IdentityConfig.cs
+++ NTime/Server/App_Start/IdentityConfig.cs
@@ -1,8 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
+using SendGrid.Helpers.Mail;
 using Server.Models;
 
 namespace Server
@@ -39,8 +46,27 @@ namespace Server
             {
                 manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
             }
+            manager.EmailService = new EmailService();
 
             return manager;
+        }
+    }
+
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            MailMessage mail = new MailMessage("rejestracja@t2w.pl", message.Destination);
+            SmtpClient client = new SmtpClient("poczta.dcsweb.pl", 587);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = false;
+            client.Credentials = new NetworkCredential("rejestracja@t2w.pl", "Ey0zx!bQw");
+
+            mail.Subject = message.Subject;
+            mail.Body = message.Body;
+            mail.IsBodyHtml = true;
+
+            return client.SendMailAsync(mail);
         }
     }
 }
