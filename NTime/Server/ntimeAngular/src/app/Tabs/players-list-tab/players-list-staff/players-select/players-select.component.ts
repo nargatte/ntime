01e40ad33c9a7f8b7d8e26e165abd8d65accd8af
chudytom <mail.chudzik@gmail.com>
2019-04-28 01:22:34 +0200

    Angular: Code cleanup after changes with component iheritance

--- NTime/Server/ntimeAngular/src/app/Tabs/players-list-tab/players-list-staff/players-select/players-select.component.ts
+++ NTime/Server/ntimeAngular/src/app/Tabs/players-list-tab/players-list-staff/players-select/players-select.component.ts
@@ -1,25 +1,14 @@
 import {
   Component,
   OnInit,
-  ViewChild,
-  AfterViewInit,
-  Input
-} from '@angular/core';
+  AfterViewInit} from '@angular/core';
 import {
-  MatPaginator,
-  MatTableDataSource,
-  MatTable,
-  PageEvent,
-  MatDialog,
-  Sort
-} from '@angular/material';
+  MatDialog} from '@angular/material';
 import { Observable } from 'rxjs';
 
 import { PlayerService } from '../../../../Services/player.service';
 import { MessageService } from '../../../../Services/message.service';
 import { PageViewModel } from '../../../../Models/PageViewModel';
-import { PlayerListView } from '../../../../Models/Players/PlayerListView';
-import { Competition } from '../../../../Models/Competitions/Competition';
 import { PlayerFilterOptions } from '../../../../Models/Players/PlayerFilterOptions';
 import { ActivatedRoute } from '@angular/router';
 import { SelectionModel } from '@angular/cdk/collections';
@@ -27,12 +16,8 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { SuccessfullActionDialogComponent } from '../../../../SharedComponents/Dialogs/successfull-action-dialog/successfull-action-dialog.component';
 import { FailedActionDialogComponent } from '../../../../SharedComponents/Dialogs/failed-action-dialog/failed-action-dialog.component';
 import { PlayersWithScores } from '../../../../Models/Players/PlayerWithScores';
-import { PlayerSort } from '../../../../Models/Enums/PlayerSort';
-import { SortHelper } from '../../../../Helpers/SortHelper';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ConfirmActionDialogComponent } from '../../../../SharedComponents/Dialogs/confirm-action-dialog/confirm-action-dialog.component';
 import { PlayersBaseListComponent } from '../../players-base-list.component';
-import { templateJitUrl } from '@angular/compiler';
 import { ExtraColumn } from '../../../../Models/ExtraColumns/ExtraColumn';
 
 @Component({
@@ -177,99 +162,4 @@ export class PlayersSelectComponent extends PlayersBaseListComponent<PlayersWith
 
     return dialogRef.afterClosed();
   }
-
-  // getFullPlayers(
-  //   competitionId: number,
-  //   playerFilterOptions: PlayerFilterOptions,
-  //   pageSize: number,
-  //   pageNumber: number
-  // ): void {
-  //   this.dataLoaded = false;
-  //   this.playerService
-  //     .getPlayersWithScores(
-  //       competitionId,
-  //       playerFilterOptions,
-  //       pageSize,
-  //       pageNumber
-  //     )
-  //     .subscribe(
-  //       (page: PageViewModel<PlayersWithScores>) => {
-  //         this.log(page.toString());
-  //         this.log(`Items: ${page.TotalCount}`);
-  //         this.players = page.Items;
-  //         this.playersCount = page.TotalCount;
-  //       },
-  //       error => this.onError(error), // Errors
-  //       () => this.setDataSource() // Success
-  //     );
-  // }
-
-  // getFullFilteredPlayers(): void {
-  //   this.dataLoaded = false;
-  //   this.getFullPlayers(
-  //     this.competitionId,
-  //     this.filter,
-  //     this.defaultPageSize,
-  //     this.defaultPageNumber
-  //   );
-  // }
-
-  // onError(message: any) {
-  //   this.dataLoaded = true;
-  //   this.messageService.addError(message);
-  // }
-
-  // log(message: string): void {
-  //   this.messageService.addLog(message);
-  // }
-
-  // setDataSource() {
-  //   this.dataSource = new MatTableDataSource<PlayersWithScores>(this.players);
-  //   this.dataLoaded = true;
-  // }
-
-  // onPageEvent(event: PageEvent) {
-  //   this.defaultPageSize = event.pageSize;
-  //   this.defaultPageNumber = event.pageIndex;
-  //   this.getFullFilteredPlayers();
-  // }
-
-  // onSortEvent(event: Sort) {
-  //   const sortOrder = SortHelper.isSortDescending(event.direction);
-  //   if (sortOrder === null) {
-  //     this.setDefaultSorting();
-  //   } else {
-  //     this.filter.DescendingSort = sortOrder;
-  //     this.filter.PlayerSort = SortHelper.getPlayerSort(event.active);
-  //     if (this.filter.PlayerSort === PlayerSort.ByExtraData) {
-  //       this.filter.ExtraDataSortIndex = Number.parseInt(event.active);
-  //     }
-  //   }
-
-  //   this.getFullFilteredPlayers();
-  // }
-
-  // prepareExtraColumns(): void {
-  //   this.competition.ExtraColumns.map(column => column.Id).forEach(id =>
-  //     this.displayedColumns.push(id.toString())
-  //   );
-  // }
-
-
-  // public search(term: string): void {
-  //   this.defaultPageNumber = 0;
-  //   debounceTime(300);
-  //   distinctUntilChanged();
-  //   this.filter.Query = term;
-  //   this.getFilteredPlayers();
-  // }
-
-
-
-  // private setPagetOptions() {
-  //   const pageOptions = this.playerService.getPaginationInfo();
-  //   this.defaultPageNumber = pageOptions.defaultPageNumber;
-  //   this.defaultPageSize = pageOptions.defaultPageSize;
-  //   this.pageSizeOptions = pageOptions.pageSizeOptions;
-  // }
 }
